5fafcac6e357a62f93fe2ae89353a62ddd51eeb4
david1707 <DavidAlacant@gmail.com>
2019-11-09 13:14:45 +0100

    Counted posts by user on computed property

--- src/views/BasicView.vue
+++ src/views/BasicView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="basic">
     <h1 class="text-center">Basic View</h1>
-    <v-data-table :headers="headers" :items="users" class="elevation-1">
+    <v-data-table :headers="headers" :items="items" class="elevation-1">
       <template v-slot:item.id="{ item }">
         <td class="text-xs-right">{{ item.id}}</td>
       </template>
@@ -49,29 +49,42 @@ export default {
         {
           sortable: true,
           text: "Website",
-          value: "website",
+          value: "website"
         },
+        {
+          sortable: true,
+          text: "Total Posts",
+          value: "total_posts"
+        }
       ]
     };
   },
   computed: {
-    ...mapGetters({ posts: "getPostsBasic", users: "getUsersBasic" })
+    ...mapGetters({ posts: "getPostsBasic", users: "getUsersBasic" }),
+    items() {
+      this.users.map(user => {
+        user.total_posts = this.posts.filter(post => post.userId === user.id).length;
+      });
+      return this.users
+    }
   },
-  methods:{
+  methods: {
     titleCase(text) {
-      return text.charAt(0).toUpperCase() + text.substr(1).toLowerCase()
+      return text.charAt(0).toUpperCase() + text.substr(1).toLowerCase();
     },
     bodyCase(text) {
-      return text.charAt(0).toUpperCase() + text.substr(1, 120).toLowerCase() + '...'
+      return (
+        text.charAt(0).toUpperCase() + text.substr(1, 120).toLowerCase() + "..."
+      );
     }
   }
 };
 </script>
 
 <style scoped>
-  .basic {
-    width: 90%;
-    margin: 0 auto;
-    padding-top: 2em;
-  }
+.basic {
+  width: 90%;
+  margin: 0 auto;
+  padding-top: 2em;
+}
 </style>
